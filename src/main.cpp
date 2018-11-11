f93dcadaa0fc38895aa076925e7dbca150c49414
Mistlight <mistlight@otakukingdom.com>
2018-11-11 13:34:32 -0500

    refactor static function access

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,17 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    app.setOrganizationName("Otakukingdom Co");
-    app.setOrganizationDomain("nodoka.otakukingdom.com");
-    app.setApplicationName("Nodoka");
+    QApplication::setOrganizationName("Otakukingdom Co");
+    QApplication::setOrganizationDomain("nodoka.otakukingdom.com");
+    QApplication::setApplicationName("Nodoka");
 
     RunGuard guard("Z0DWjf33Am1YeCUdIW7h0vSxjU2RJjZcUqzgG ver0.0.2a");
 
 
     if(!guard.tryToRun()) {
-        QMessageBox *messageBox = new QMessageBox();
-        messageBox->critical(0, "Error", "Cannot launch multiple instances of Nodoka Player");
+        new QMessageBox();
+        QMessageBox::critical(nullptr,
+                "Error", "Cannot launch multiple instances of Nodoka Player");
 
         return EXIT_FAILURE;
     }
@@ -26,8 +27,9 @@ int main(int argc, char *argv[]) {
 
 
     if(!Core::openDb()) {
-        QMessageBox *messageBox = new QMessageBox();
-        messageBox->critical(0, "Error", "Failed to load the config file");
+        new QMessageBox();
+        QMessageBox::critical(nullptr,
+                "Error", "Failed to load the config file");
 
         // since we failed to load the db, we shouldn't continue
         return EXIT_FAILURE;
@@ -37,5 +39,5 @@ int main(int argc, char *argv[]) {
         nodokaApp.start();
     }
 
-    return app.exec();
+    return QApplication::exec();
 }
